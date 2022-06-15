d6f8d1f8c6b3b9befb28a0993da9cca191d3c323
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-06-15 11:53:58 +0530

    Adding react files

---
+++ day-1-react-js/jay/src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+import { Hello, Bye } from './funciton';
+import Ok from './funciton';  
+
+export default function App()
+{
+
+  return (
+    <div className="App">
+      Jayesh
+      <Hello /> 
+      <Bye />
+      <Ok />
+    </div>
+  );
+}
---
+++ day-1-react-js/jay/src/function.js
@@ -0,0 +1,23 @@
+export function Hello(){
+    return (
+        <div>
+            Hello world
+        </div>
+    )
+}
+
+export function Bye(){
+    return (
+        <div>
+            Bye
+        </div>
+    )
+}
+
+export default function Ok(){
+    return (
+        <div>
+            OK
+        </div>
+    )
+}
