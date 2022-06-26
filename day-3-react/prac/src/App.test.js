41be135abf7e3bf2114e1cb90cf3730a9fdaf709
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-06-27 03:11:47 +0530

    Adding some more components

--- day-3-react/prac/src/App.test.js
+++ day-3-react/prac/src/App.test.js
@@ -1,13 +1,27 @@
 import { render, screen } from '@testing-library/react';
+import Quickstart from './quickstart';
 import App from './App';
+import FloatingActionButtons from './floatbuttons.js';
 import BasicSelect from './autocomplete.js';
 import ColorCheckboxes from './checkbox.js';
 import Avatars2 from  './avatar.js';
+import ContinuousSlider from './slider.js';
+import SimpleBackdrop from './backdrop.js';
 
-
+test('renders learn react link', () => {
+  render(<Quickstart />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
 test('renders learn react link', () => {
   render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<FloatingActionButtons />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
@@ -26,6 +40,18 @@ test('renders learn react link', () => {
 
 test('renders learn react link', () => {
   render(<Avatars2 />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<ContinuousSlider />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<SimpleBackdrop />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- day-3-react/prac/src/index.js
+++ day-3-react/prac/src/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Quickstart from './quickstart';
 import App from './App';
+import FloatingActionButtons from './floatbuttons.js';
 import reportWebVitals from './reportWebVitals';
 import BasicSelect from './autocomplete.js';
 import ColorCheckboxes from './checkbox.js';
 import Avatars2 from  './avatar.js';
+import ContinuousSlider from './slider.js';
+import SimpleBackdrop from './backdrop.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Quickstart />
     <App />
+    <FloatingActionButtons />
     <BasicSelect />
     <ColorCheckboxes />
     <Avatars2 />
+    <ContinuousSlider />
+    <SimpleBackdrop />
   </React.StrictMode>
 );
 
