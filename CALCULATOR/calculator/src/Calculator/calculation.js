f25b0e3253d40111fe919b218e1264a0bbc62f96
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-06-30 10:02:56 +0530

    modification

--- CALCULATOR/calculator/src/Calculator/calculation.js
+++ CALCULATOR/calculator/src/Calculator/calculation.js
@@ -3,9 +3,9 @@ import React, { useState } from "react";
 function Calc() {
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
-  const [op,setOp] = useState();
+  const [op, setOp] = useState();
 
-  const [result,setResult] = useState(0);
+  const [result, setResult] = useState(0);
 
   function handleChange(event, number) {
     setNum1(event.target.value);
@@ -13,27 +13,37 @@ function Calc() {
   function handleChange1(event, number) {
     setNum2(event.target.value);
   }
-  function handleChange2(event) {
-      setOp(event.target.value);
+
+  //function handleSelect(e) {
+  //  setOp(e.target.value);
+  //}
+
+  function handleSelect(str) {
+    setOp(str);
   }
+
   function calculate() {
+    console.log(op);
     let ans = 0;
-     if (op === "+") {
-        ans = Number(num1) + Number(num2);
-     }else if (op === "-") {
-        ans = Number(num1) - Number(num2);
-     }else if (op === "*") {
-        ans = Number(num1) * Number(num2);
-     }else {
-        ans = Number(num1) / Number(num2);
-     }
-     setResult(ans);
+    if (op === "+") {
+      ans = Number(num1) + Number(num2);
+    }
+    else if (op === "-") {
+      ans = Number(num1) - Number(num2);
+    }
+    else if (op === "*") {
+      ans = Number(num1) * Number(num2);
+    }
+    else if (op === "/") {
+      ans = Number(num1) / Number(num2);
+    }
+    setResult(ans);
   }
 
   return (
     <div className="cal">
       <label>Enter first number</label>
-      <input 
+      <input
         className="number1"
         type="number"
         name="num1"
@@ -42,7 +52,7 @@ function Calc() {
       />
 
       <label>Enter second number</label>
-      <input 
+      <input
         className="number2"
         type="number"
         name="num2"
@@ -50,18 +60,20 @@ function Calc() {
         onChange={(event) => handleChange1(event, num2)}
       />
 
-      <label>Enter operator</label>
+      <label className="lab3">Enter operator</label>
 
-      <input 
-      type="operator"
-      value={op}
-      onChange={(event) => handleChange2(event,op)}
-      />
+      <select name="operator" onChange={(str) => handleSelect(str)}>
+        <option value="" onChange={() => handleSelect('+')}>+</option>
+        <option value="" onChange={() => handleSelect('-')}>-</option>
+        <option value="" onChange={() => handleSelect('*')}>*</option>
+        <option value="" onChange={() => handleSelect('/')}>/</option>
+      </select>
 
-      <button className="but1">+</button>
-      <button className="but2">-</button>
-      <button className="but3">*</button>
-      <button className="but4">/</button>
+      {/*<button className="but1" onClick={() => handleSelect('+')}>+</button>
+      <button className="but2" onClick={() => handleSelect('-')}>-</button>
+      <button className="but3" onClick={() => handleSelect('*')}>*</button>
+      <button className="but4" onClick={() => handleSelect('/')}>/</button>*/}
+
       <button type="submit" onClick={calculate}>
         Calculate
       </button>
