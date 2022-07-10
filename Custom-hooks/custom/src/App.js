0f2608da6875937525701202db6d837fbc41fa41
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-07-10 20:33:15 +0530

    modifying app.js and adding table.js

--- Custom-hooks/custom/src/App.js
+++ Custom-hooks/custom/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { useState , useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import Table from './table';
 import './App.css';
-function useTable(){
-  const [data,setData] = useState([]); 
-  const [loading,setLoading] = useState(false);
-  const [error,setError] = useState(false);
+function useTable() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  async function tabledata(){
+  async function tabledata() {
     setLoading(true);
     setError(false);
 
@@ -17,20 +18,21 @@ function useTable(){
     } catch (error) {
       setError(true);
     }
-      setLoading(false);
+    setLoading(false);
   }
   useEffect(() => {
     tabledata();
-  },[]);
-  return [{loading,data,error},tabledata]
+  }, []);
+  return [{ loading, data, error }, tabledata]
 }
 function App() {
-  const [loading,data,error] = useTable("https://jsonplaceholder.typicode.com/users");
+  const [loading, data, error] = useTable("https://jsonplaceholder.typicode.com/users");
   return (
     <div className="App">
-      {loading ? <img src = "https://i.stack.imgur.com/ATB3o.gif" alt = "Sorry" /> : <p>{JSON.stringify(data)}</p>}
+      {loading ? <img src="https://i.stack.imgur.com/ATB3o.gif" alt="Sorry" /> : <p>{JSON.stringify(data)}</p>}
 
-      {error ? "Error" : <></>} 
+      {error ? "Error" : <></>}
+      <Table />
     </div>
   );
 }
